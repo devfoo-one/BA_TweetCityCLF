802eceeb1f32755522208bf80fbd99a22ad709b9
Tom Oberhauser <tom@devfoo.de>
2016-07-07 19:25:32 +0200

    Tokenizer Fortschritt

--- Utils/tokenizer.py
+++ Utils/tokenizer.py
@@ -1,22 +1,24 @@
-from Utils import happyfuntokenizing
-
+import re
 
 class Tokenizer:
     __remove_urls = False
     __remove_user_mentions = False
     __preserve_case = True
     __remove_hashtags = False
+    __blind_urls = False
 
-    def __init__(self, remove_urls=False, remove_user_mentions=False, remove_hashtags=False, preserve_case=True):
+    def __init__(self, blind_urls=False, remove_urls=False, remove_user_mentions=False, remove_hashtags=False,
+                 preserve_case=True):
         self.__remove_urls = remove_urls
         self.__remove_user_mentions = remove_user_mentions
         self.__preserve_case = preserve_case
         self.__remove_hashtags = remove_hashtags
-        # self.__hft__ = happyfuntokenizing.Tokenizer(preserve_case=self.__preserve_case__)
+        self.__blind_urls = blind_urls
 
     def tokenize(self, tweet):
         tweet_text = tweet['text']
 
+        """remove URLs"""
         if self.__remove_urls:
             # collect all url strings (ugly but works)
             urls = []
@@ -30,17 +32,31 @@ class Tokenizer:
             for url in urls:
                 tweet_text = tweet_text.replace(url, '')
 
+        """blind URLs"""
+        if self.__blind_urls:
+            for url in tweet['entities']['urls']:
+                tweet_text = tweet_text.replace(url['url'], '[URL]')
+            try:
+                for url in tweet['entities']['media']:
+                    tweet_text = tweet_text.replace(url['url'], '[MEDIA_URL]')
+            except KeyError:
+                pass
+
+        """remove user mentions"""
         if self.__remove_user_mentions:
             for mention in tweet['entities']['user_mentions']:
                 tweet_text = tweet_text.replace('@' + mention['screen_name'], '')
 
+        """remove hashtags"""
         if self.__remove_hashtags:
             for hashtag in tweet['entities']['hashtags']:
                 tweet_text = tweet_text.replace('#' + hashtag['text'], '')
 
-        tokens = tweet_text.split()
+        """tokenization, only whitespace for now..."""
+        word_re = re.compile(r'[\s]+')
+        tokens = word_re.split(tweet_text)
 
         if not self.__preserve_case:
-            pass
+            tokens = map(lambda x: str(x).lower(), tokens)
 
         return tokens
--- playground/playground_tokenizer.py
+++ playground/playground_tokenizer.py
@@ -1,11 +1,11 @@
 import json
 from Utils import tokenizer
 
-tok = tokenizer.Tokenizer(remove_urls=True, remove_user_mentions=True, remove_hashtags=True, preserve_case=False)
+# tok = tokenizer.Tokenizer(remove_urls=True, remove_user_mentions=True, remove_hashtags=True, preserve_case=True)
+tok = tokenizer.Tokenizer(blind_urls=True, remove_user_mentions=True, remove_hashtags=False, preserve_case=True)
 with open('../../data/Germany_filtered_shuffled.json') as dataset:
     for line in dataset:
         tweet = json.loads(line)
         print('---', tweet['text'], '---')
-        for token in tok.tokenize(tweet):
-            print(token)
-            pass
+        tokens = tok.tokenize(tweet)
+        print(tokens)
