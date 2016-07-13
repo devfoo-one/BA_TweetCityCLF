acc88460f0f77445f9fac37977578ee02df23158
Tom Oberhauser <tom@devfoo.de>
2016-07-13 19:10:49 +0200

    Preproc: preserve_case -> transform_lowercase

--- Utils/tweet_preprocessor.py
+++ Utils/tweet_preprocessor.py
@@ -2,25 +2,25 @@ import re
 
 
 class Processor:
-    __remove_urls = False
-    __remove_user_mentions = False
-    __preserve_case = True
-    __remove_hashtags = False
-    __blind_urls = False
+    __remove_urls__ = False
+    __remove_user_mentions__ = False
+    __transform_lowercase__ = True
+    __remove_hashtags__ = False
+    __blind_urls__ = False
 
     def __init__(self, blind_urls=False, remove_urls=False, remove_user_mentions=False, remove_hashtags=False,
-                 preserve_case=True):
-        self.__remove_urls = remove_urls
-        self.__remove_user_mentions = remove_user_mentions
-        self.__preserve_case = preserve_case
-        self.__remove_hashtags = remove_hashtags
-        self.__blind_urls = blind_urls
+                 transform_lowercase=True):
+        self.__remove_urls__ = remove_urls
+        self.__remove_user_mentions__ = remove_user_mentions
+        self.__transform_lowercase__ = transform_lowercase
+        self.__remove_hashtags__ = remove_hashtags
+        self.__blind_urls__ = blind_urls
 
     def digest(self, tweet):
         tweet_text = tweet['text']
 
         """remove URLs"""
-        if self.__remove_urls:
+        if self.__remove_urls__:
             # collect all url strings (ugly but works)
             urls = []
             for url in tweet['entities']['urls']:
@@ -34,7 +34,7 @@ class Processor:
                 tweet_text = tweet_text.replace(url, '')
 
         """blind URLs"""
-        if self.__blind_urls:
+        if self.__blind_urls__:
             for url in tweet['entities']['urls']:
                 tweet_text = tweet_text.replace(url['url'], '[URL]')
             try:
@@ -44,16 +44,16 @@ class Processor:
                 pass
 
         """remove user mentions"""
-        if self.__remove_user_mentions:
+        if self.__remove_user_mentions__:
             for mention in tweet['entities']['user_mentions']:
                 tweet_text = tweet_text.replace('@' + mention['screen_name'], '')
 
         """remove hashtags"""
-        if self.__remove_hashtags:
+        if self.__remove_hashtags__:
             for hashtag in tweet['entities']['hashtags']:
                 tweet_text = tweet_text.replace('#' + hashtag['text'], '')
 
-        if not self.__preserve_case:
+        if self.__transform_lowercase__:
             tweet_text = tweet_text.lower()
 
         return tweet_text
