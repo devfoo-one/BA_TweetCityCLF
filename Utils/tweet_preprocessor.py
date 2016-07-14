a3708d83ab76251a547664d929c48062dd976f0c
Tom Oberhauser <tom@devfoo.de>
2016-07-14 18:50:48 +0200

    PreProc: Link Expandierung

--- Utils/tweet_preprocessor.py
+++ Utils/tweet_preprocessor.py
@@ -7,14 +7,16 @@ class Processor:
     __transform_lowercase__ = True
     __remove_hashtags__ = False
     __blind_urls__ = False
+    __expand_urls__ = True
 
     def __init__(self, blind_urls=False, remove_urls=False, remove_user_mentions=False, remove_hashtags=False,
-                 transform_lowercase=True):
+                 transform_lowercase=True, expand_urls=True):
         self.__remove_urls__ = remove_urls
         self.__remove_user_mentions__ = remove_user_mentions
         self.__transform_lowercase__ = transform_lowercase
         self.__remove_hashtags__ = remove_hashtags
         self.__blind_urls__ = blind_urls
+        self.__expand_urls__ = expand_urls
 
     def digest(self, tweet):
         """Processes a tweet object (as given from the streaming api) and returns a string."""
@@ -54,8 +56,22 @@ class Processor:
             for hashtag in tweet['entities']['hashtags']:
                 tweet_text = tweet_text.replace('#' + hashtag['text'], '')
 
+        """transform to lowercase"""
         if self.__transform_lowercase__:
             tweet_text = tweet_text.lower()
+
+        """expand urls"""
+        if self.__expand_urls__:
+            urls = []
+            for url in tweet['entities']['urls']:
+                urls.append((url['url'], url['expanded_url']))
+            try:
+                for url in tweet['entities']['media']:
+                    urls.append((url['url'], url['expanded_url']))
+            except KeyError:
+                pass
+            for url, expanded_url in urls:
+                tweet_text = tweet_text.replace(url, expanded_url)
 
         return tweet_text
 
