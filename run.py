1f902b0f77ae42abbb219fb5c918ea87191bc354
Tom Oberhauser <tom@devfoo.de>
2016-08-01 22:28:55 +0200

    Finished experiment 1 and 2 to show the significance of the 'other' target class

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import sys
 
-import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn import metrics
 from sklearn.naive_bayes import MultinomialNB
@@ -24,31 +23,83 @@ tok = tweet_tokenizer.Tokenizer()
     |               80% TRAIN            | 10% TEST | 10% VALI |
      ----------------------------------------------------------
 """
-raw_train_data_nlt, train_targets_nlt = dataset.getData(n=len(dataset) * 0.8,
+PERCENT_TRAIN = 0.8
+PERCENT_TEST = 0.1
+PERCENT_EVAL = 0.1
+
+raw_train_data, train_targets = dataset.getData(n=len(dataset) * PERCENT_TRAIN,
+                                                cut_long_tail=False)  # get 80% of data for training
+raw_train_data_nlt, train_targets_nlt = dataset.getData(n=len(dataset) * PERCENT_TRAIN,
                                                         cut_long_tail=True)  # get 80% of data for training
-raw_test_data_nlt, test_targets_nlt = dataset.getData(offset=len(dataset) * 0.8, n=len(dataset) * 0.1,
+raw_test_data, test_targets = dataset.getData(offset=len(dataset) * PERCENT_TRAIN, n=len(dataset) * PERCENT_TEST,
+                                              cut_long_tail=False)  # get another 10% for testing
+raw_test_data_nlt, test_targets_nlt = dataset.getData(offset=len(dataset) * PERCENT_TRAIN,
+                                                      n=len(dataset) * PERCENT_TEST,
                                                       cut_long_tail=True)  # get another 10% for testing
 
-"""
+
+def e1():
+    """
     E1 - Binary BagOfWords
     Full text gets tokenized and transformed into a binary term-document-matrix.
-    Dataset without long tail
-"""
-print('---------- e1: BINARY BOW BASELINE BEGIN ----------')
-e1_preproc_text = tp.TextProcessor(blind_urls=False, remove_urls=False, remove_user_mentions=False,
-                                   remove_hashtags=False,
-                                   transform_lowercase=False, expand_urls=False)
-print('** preproc config:', e1_preproc_text, '**')
-print('Training classifier...', end='', flush=True)
-clf_e1 = Pipeline([('vect', CountVectorizer(preprocessor=e1_preproc_text, tokenizer=tok, lowercase=False, binary=True)),
-                   ('clf', MultinomialNB()),
-                   ])
-clf_e1.fit(raw_train_data_nlt, train_targets_nlt)
-print('done.')
-print('Predicting test data...', end='', flush=True)
-e1_predicted = clf_e1.predict(raw_test_data_nlt)
-print('done.')
-labels = list(set(test_targets_nlt))  # take only labels that have support
-target_names = [dataset.getTargetName(x) for x in labels]
-print(metrics.classification_report(test_targets_nlt, e1_predicted, labels=labels, target_names=target_names, digits=4))
-print('---------- e1: BINARY BOW BASELINE END ----------')
+    Dataset with long tail
+    """
+    print('========== e1: BINARY BOW BASELINE WITH LONG TAIL BEGIN ==========')
+    e1_preproc_text = tp.TextProcessor(blind_urls=False, remove_urls=False, remove_user_mentions=False,
+                                       remove_hashtags=False,
+                                       transform_lowercase=False, expand_urls=False)
+    print('** preproc config:', e1_preproc_text, '**')
+    print('Training classifier...', end='', flush=True)
+    pipeline_e1 = Pipeline(
+        [('vect', CountVectorizer(preprocessor=e1_preproc_text, tokenizer=tok, lowercase=False, binary=True)),
+         ('clf', MultinomialNB()),
+         ])
+    pipeline_e1.fit(raw_train_data, train_targets)
+    print('done.')
+    print('Predicting test data...', end='', flush=True)
+    e1_predicted = pipeline_e1.predict(raw_test_data)
+    print('done.')
+    print('--- FULL CLASSIFICATION REPORT WITH ALL CLASSES ---')
+    labels = list(set(test_targets))  # take only labels that have support
+    target_names = [dataset.getTargetName(x) for x in labels]
+    print(metrics.classification_report(test_targets, e1_predicted, labels=labels, target_names=target_names, digits=4))
+    print('--- CLASSIFICATION REPORT FOR LONG-TAIL CLASSES ---')
+    labels_longtail = list(set(test_targets).difference(set(test_targets_nlt)))  # take only labels that have support
+    target_names_longtail = [dataset.getTargetName(x) for x in labels]
+    print(metrics.classification_report(test_targets, e1_predicted, labels=labels_longtail,
+                                        target_names=target_names_longtail, digits=4))
+    print('========== e1: BINARY BOW BASELINE END ==========')
+
+
+def e2():
+    """
+    E2 - Binary BagOfWords without long-tail
+    Full text gets tokenized and transformed into a binary term-document-matrix.
+    Dataset with long tail
+    """
+    print('========== e2: BINARY BOW BASELINE WITHOUT LONG-TAIL BEGIN ==========')
+    e2_preproc_text = tp.TextProcessor(blind_urls=False, remove_urls=False, remove_user_mentions=False,
+                                       remove_hashtags=False,
+                                       transform_lowercase=False, expand_urls=False)
+    print('** preproc config:', e2_preproc_text, '**')
+    print('Training classifier...', end='', flush=True)
+    pipeline_e2 = Pipeline(
+        [('vect', CountVectorizer(preprocessor=e2_preproc_text, tokenizer=tok, lowercase=False, binary=True)),
+         ('clf', MultinomialNB()),
+         ])
+    pipeline_e2.fit(raw_train_data_nlt, train_targets_nlt)
+    print('done.')
+    print('Predicting test data...', end='', flush=True)
+    e2_predicted = pipeline_e2.predict(raw_test_data_nlt)
+    print('done.')
+    print('--- FULL CLASSIFICATION REPORT WITH ALL CLASSES ---')
+    labels = list(set(test_targets_nlt))  # take only labels that have support
+    target_names = [dataset.getTargetName(x) for x in labels]
+    print(metrics.classification_report(test_targets_nlt, e2_predicted, labels=labels, target_names=target_names,
+                                        digits=4))
+    print('========== e2: BINARY BOW BASELINE WITHOUT LONG-TAIL END ==========')
+
+
+"""Run experiments"""
+e1()
+e2()
