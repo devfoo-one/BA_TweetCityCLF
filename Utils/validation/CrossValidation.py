1ac6c61533b235e7361d11371152d87d6e8e9c1a
Tom Oberhauser <tom@devfoo.de>
2016-08-23 23:42:50 +0200

    Cross validation test

---
+++ Utils/validation/CrossValidation.py
@@ -0,0 +1,36 @@
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+
+
+# http://stackoverflow.com/questions/35876508/evaluate-multiple-scores-on-sklearn-cross-val-score
+
+class PrintingScorer:
+    """
+    A scorer for scikit-learn cross validation needs to be an instance of a class since scikit is internally pickling stuff.
+    This seems to be necessary for threading.
+    """
+
+    def __init__(self):
+        pass
+
+    def __call__(self, estimator, X, y):
+        """
+        According to sklearn documentation, this is how an scorer method is build.
+        This is necessary because sklearn does not allow calculating more than one score in one run :(
+        Printing those scores is a dirty workaround, but it works for now.
+        :param estimator: Estimator to test
+        :param X: data
+        :param y: ground truth
+        :return: f1_weighted score, this is necessary for comparison if this scorer is used in a grid search.
+        """
+        y_predicted = estimator.predict(X)
+        precision = precision_score(y, y_predicted)
+        recall = recall_score(y, y_predicted)
+        f1_micro = f1_score(y, y_predicted, average='micro')
+        f1_macro = f1_score(y, y_predicted, average='macro')
+        f1_weighted = f1_score(y, y_predicted, average='weighted')
+        print(
+            "Precision: {}, Recall: {}, F1_micro: {}, F1_macro: {}, F1_weighted: {}".format(precision, recall, f1_micro,
+                                                                                            f1_macro, f1_weighted))
+        return f1_weighted
--- playground/playground_xval_test.py
+++ playground/playground_xval_test.py
@@ -8,19 +8,20 @@ from Dataset import Dataset
 from sklearn import cross_validation
 from Utils.preprocessing import preproc_text as tp
 from Utils.tokenization import Tokenize
+from Utils.validation import CrossValidation
 
-raw_train_data, train_targets = Dataset(dataset=sys.argv[1]).getData(cut_long_tail=True)
+dataset = Dataset(dataset=sys.argv[1])
+raw_train_data, train_targets = dataset.getData(cut_long_tail=True)
 tok = Tokenize.TweetTokenizer()
 preproc_text = tp.TextProcessor(blind_urls=False, remove_urls=False, remove_user_mentions=False,
-                                    remove_hashtags=False,
-                                    transform_lowercase=False, expand_urls=False)
+                                remove_hashtags=False,
+                                transform_lowercase=False, expand_urls=False)
 print('** preproc config:', preproc_text, '**')
 print('Training classifier...', end='', flush=True)
 pipeline = Pipeline(
     [('vect', CountVectorizer(preprocessor=preproc_text, tokenizer=tok, lowercase=False, binary=True)),
      ('clf', MultinomialNB()),
      ])
-print('--- 10-FOLD CV, WEIGHTED F1 ---')
-scores = cross_validation.cross_val_score(pipeline, raw_train_data, train_targets, cv=10, n_jobs=-1,
-                                          scoring="f1_weighted")
-print(scores)
+print('--- 5-FOLD CV, WEIGHTED F1 ---')
+scorer = CrossValidation.PrintingScorer()
+cross_validation.cross_val_score(pipeline, raw_train_data, train_targets, cv=5, n_jobs=5, scoring=scorer)
